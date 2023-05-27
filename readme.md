3b829119447691c0a0e004c2f6795e19b52ca9b1
Daniel Sávio <spnjc137@gmail.com>
2023-05-27 17:57:09 -0300

    chore: the basics for creating the identity of the project

---
+++ readme.md
@@ -0,0 +1,7 @@
+Detalhes do projeto
+
+- Selecionar fundo: Cor sólida e imagem;
+- Selecionar fonte;
+- Selecionar dimensões da placa gerada;
+- Quebra de texto (Enter ou como preferir);
+- Indicar se o texto é muito grande pro tamanho da placa selecionado.
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useRef, ReactElement  } from 'react'
+import { exportComponentAsPNG } from 'react-component-export-image';
+
 
 function App() {
-  return (
-    <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  const [text, setText] = useState<String>('Default');
+  const componentRef = useRef<HTMLInputElement>(null);
 
-    </>
+  return (
+    <main className=' bg-slate-50 h-screen w-screen flex flex-col items-center justify-center'>
+      <h1 className="text-3xl font-bold text-center underline w-full">
+        Planeta acessível
+      </h1>
+
+      <div ref={componentRef} className='flex flex-col items-center justify-center bg-slate-300 w-3/4 h-80 rounded-md drop-shadow-lg'>
+        <input className='text-gray-900 text-3xl rounded-lg text-center focus:outline-none bg-transparent block  ' type="text" name="text" id="text" placeholder='text' onChange={(e) => { setText(e.target.value) }} />
+        <br />
+        <br />
+        <span className='text-5xl'> {text}</span>
+      </div>
+
+      <button 
+      onClick={(e) => {exportComponentAsPNG(componentRef)}}
+      className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'>
+        Baixar PDF
+      </button>
+
+    </main>
   )
 }
 
